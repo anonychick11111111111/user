efd62d4a0764cea49f60cf55fb29ec8ee54a8577
anonychick11111111111 <cuong.dd187593@sis.hust.edu.vn>
2022-08-07 13:38:47 +0700

    user, service files

---
+++ delete.js
@@ -0,0 +1,19 @@
+// changeTitle = (btn) => {
+//    const trNode = btn.parentElement;
+   
+//    const span = trNode.childNodes[1];
+//    console.log(span)
+   
+  
+//    span.remove();
+
+//    const para = document.createElement('div');
+//    para.innerHTML=`<div class="form-outline col-lg-5 ">
+//    <input type="text" id="form12" class="form-control" />
+//    <label class="form-label" for="form12">Example label</label>
+//  </div>`;
+
+//  trNode.insertBefore(para, btn);
+//    console.log(trNode);
+   
+// }
