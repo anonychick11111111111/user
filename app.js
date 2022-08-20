c3c25745a9b207002645b8a80db7c9484dae90ae
anonychick11111111111 <cuong.dd187593@sis.hust.edu.vn>
2022-08-20 16:27:07 +0700

    20/8

--- app.js
+++ app.js
@@ -20,15 +20,27 @@
 
 function deletee(btn) {
     const parentNode = btn.parentElement.parentElement;
-
-    const name = parentNode.childNodes[5];
-    console.log(name);
+    const name = parentNode.childNodes[5];;
     const nameP = name.childNodes[3];
     nameP.remove();
     const para = document.createElement('div');
-    para.classList.add('form-outline');
-    para.innerHTML=`<input type="text" id="form12" class="form-control" />
-    <label class="form-label" for="form12">input</label>`;
+    para.classList.add('#form-outline');
+    para.innerHTML=`<input type="text" id="form12" class="form-control" oninput="getValue(this)"/>
+    <label class="form-label" for="form12"></label>`;
     name.appendChild(para);
+
+    const button = parentNode.childNodes[7];
+    button.remove();
 }
+
+function getValue(input) {
+    const parentNode = input.parentElement.parentElement;
+    const inputValue = parentNode.childNodes[4].childNodes[0].value;
+    console.log(parentNode.childNodes[4]);
+    const array = [];
+    array.push(inputValue);
+    console.log(array);
+}
+
+
 
